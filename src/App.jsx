af5304597396f87fa7b50231dc978624ad896daa
Harsh Dhiman <harshdhiman484@gmail.com>
2025-05-05 11:55:27 +0530

    upgarde

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './Header'
+import HeroSection from './HeroSection'
 function App() {
 
   return (
     <>
     <Header></Header>
+    <HeroSection />
     </>
   )
 }
--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function HeroSection() {
   return (
     <>
-    <div>
+      <section className="flex justify-between border-3 ">
+        <div className="border-red-700">
+          <img src="https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png"></img>
+        </div>
 
-<img src=''></img>
-
-<div>
-<h4>Hello ,I'm</h4>
-<h1>Harsh</h1>
-<h4></h4>
-</div>
-    </div>
-    
-    
-    
+        <div className="border-red-700">
+          <h3>Hello i'm</h3>
+          <h1>Harsh</h1>
+          <h3>lorem texxt-33</h3>
+          <div>
+            <a href="#/" >Download CV</a>
+            <a href="#/">Contact Info</a>
+          </div>
+        </div>
+        <div></div>
+      </section>
     </>
-  )
+  );
 }
