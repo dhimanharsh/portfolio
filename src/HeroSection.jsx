37f7ba8bfdf4a1eaf4cb178fdd7fd3ad75277fd3
Harsh Dhiman <harshdhiman484@gmail.com>
2025-05-06 11:39:43 +0530

    adding styles

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -3,22 +3,35 @@ import React from "react";
 export default function HeroSection() {
   return (
     <>
-      <section className="flex justify-between border-3 ">
-        <div className="border-red-700">
-          <img src="https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png"></img>
-        </div>
+<section className="bg-red-500 h-200 w-[90%] mx-auto flex items-center">
 
-        <div className="border-red-700">
-          <h3>Hello i'm</h3>
-          <h1>Harsh</h1>
-          <h3>lorem texxt-33</h3>
-          <div>
-            <a href="#/" >Download CV</a>
-            <a href="#/">Contact Info</a>
-          </div>
-        </div>
-        <div></div>
-      </section>
+<div className="border-3 h-[100%] w-[50%]">
+
+</div>
+
+<div className="h-[40%] border-4 w-[50%] flex flex-col justify-around items-center">
+
+<div className="">
+  <h2 className="text-[2.5rem]">Hello I'm</h2>
+  <h1 className="text-[3rem]">Harsh Dhiman</h1>
+  <h2>FrontEnd - Developer</h2>
+</div>
+<div>
+<a href="#">Download Cv</a>
+<a href="#">Contact Info</a>
+</div>
+<div>
+<button>linledin</button>
+<button>github</button>
+</div>
+
+
+</div>
+
+
+</section>
+
+
     </>
   );
 }
